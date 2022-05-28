f87edd8c88d485f2197f0e182ca00ea80f3e71d7
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-05-28 07:35:28 +0400

    Implement add to watchlist

--- src/app.js
+++ src/app.js
@@ -7,14 +7,23 @@ document.addEventListener('alpine:init', () => {
         message: null
       },
       timeout: null,
+      watchlistCount: 0,
       addToCart() {
         this.cartItemCount++;
+        this.showNotification(`The Item was successfully added into your cart`)
+      },
+      addToWatchlist() {
+        this.watchlistCount++;
+        this.showNotification(`The Item was successfully added in your watchlist`)
+      },
+      showNotification(message) {
         this.notification.show = true;
-        this.notification.message = `The Item was successfully added into your cart`;
+        this.notification.message = message;
         clearTimeout(this.timeout);
         this.timeout = setTimeout(() => {
           this.notification.show = false;
         }, 3000)
       }
+
   }))
 })
